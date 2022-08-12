77e5a6d4fe184280f4726e3716d1adf078f161ac
redMulla <ivassillycharite@gmail.com>
2022-08-13 00:01:48 +0300

    sidebar social media

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faTwitter, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -24,6 +25,28 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/vassilly-red-v-ibinkwiye-3884131b9/'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/redMulla'>
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://twitter.com/Vassilly3'>
+                    <FontAwesomeIcon icon={faTwitter} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://twitter.com/Vassilly3'>
+                    <FontAwesomeIcon icon={faWhatsapp} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
     </div>
   )
 }
