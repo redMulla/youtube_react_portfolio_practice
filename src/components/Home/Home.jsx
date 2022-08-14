3cad6554f740634fa9a82074958d55f0ffec5c67
redMulla <ivassillycharite@gmail.com>
2022-08-14 19:45:48 +0300

    Logo components svg 1st touch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
     const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
     const jobArray = ['w','e','b',' ', 'd','e','v','e','l','o','p','e','r']
     const timeOutAnimate = () => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 4000)
+      return setTimeout(() => {
+        setLetterClass('text-animate-hover')
+      }, 4000)
     }
     useEffect(() => {
         timeOutAnimate()
@@ -20,28 +20,28 @@ const Home = () => {
 
   return (
     <div className='container home-page'>
-        <div className="text-zone">
-            <h1>
-            <span className={letterClass}>H</span>  
-            <span className={`${letterClass} _12`}>i,</span> 
-            <br />
-            <span className={`${letterClass} _13`}>I</span> 
-            <span className={`${letterClass} _14`}>'m</span> 
-            <img src={LogoTitle} alt="developer" />
-            <AnimatedLetters letterClass={letterClass}
-              strArray= {nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters letterClass={letterClass}
-              strArray= {jobArray}
-              idx={22}
-            />
-            </h1>
-            <h2>Frontend Developer / JavaScript expert / YouTuber</h2>
-            <Link to='/contact' className='flat-button'>CONTACT ME</Link>
-        </div>
-        <Logo />
+      <div className="text-zone">
+        <h1>
+        <span className={letterClass}>H</span>  
+        <span className={`${letterClass} _12`}>i,</span> 
+        <br />
+        <span className={`${letterClass} _13`}>I</span> 
+        <span className={`${letterClass} _14`}>'m</span> 
+        <img src={LogoTitle} alt="developer" />
+        <AnimatedLetters letterClass={letterClass}
+          strArray= {nameArray}
+          idx={15}
+        />
+        <br />
+        <AnimatedLetters letterClass={letterClass}
+          strArray= {jobArray}
+          idx={22}
+        />
+        </h1>
+        <h2>Frontend Developer / JavaScript expert / YouTuber</h2>
+        <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+      </div>
+      <Logo />
     </div>
   )
 }
