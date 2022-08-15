5b281815ca0bc05a59c5a73acb53194d101d4aac
redMulla <ivassillycharite@gmail.com>
2022-08-15 09:30:04 +0300

    About h1 css style

--- src/components/Aboout/About.jsx
+++ src/components/Aboout/About.jsx
@@ -14,6 +14,20 @@ const About = () => {
           />
               
         </h1>
+        <p>
+            I'm very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
       </div>
     </div>
   )
