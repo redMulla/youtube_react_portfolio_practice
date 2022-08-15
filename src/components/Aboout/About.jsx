9c828713dcac6f91bfe26a02ec255c34377c975d
redMulla <ivassillycharite@gmail.com>
2022-08-15 19:38:11 +0300

    about animation implementation part 2

--- src/components/Aboout/About.jsx
+++ src/components/Aboout/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     const animationTimeout = () => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
-        }, 4000)
+        }, 3000)
     }
 
     useEffect(() => {
@@ -17,7 +17,7 @@ const About = () => {
 
   return (
     <div className='container about-page'>
-      <div className="text-zone">
+      <div className="text-zone unique">
         <h1>
           <AnimatedLetters
             letterClass={letterClass}
