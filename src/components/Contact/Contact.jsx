4e1b22f5a19082a60b7c6cfe299fee9d36f49e74
redMulla <ivassillycharite@gmail.com>
2022-08-17 17:14:49 +0300

    contact page creation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,34 @@
 import Loader from 'react-loaders'
 import './Contact.scss';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+    const animationTimeout = () => {
+      setTimeout(() => {
+        setLetterClass('text-animate-hover')
+      }, 3000)
+    }
+
+    useEffect(() => {
+      return animationTimeout()
+    }, [])
+
   return (
     <>
         <div className='container contact-page'>
-            <div className="text-zone">
-                <h1>
-                    
-                </h1>
-            </div>
-        </div>
-        <Loader type='pacman' />
+          <div className="text-zone">
+            <h1>
+              <AnimatedLetters strArray={['C', 'o', 'n','t','a','c','t', ' ','m','e']}
+                idx={15}
+                letterClass={letterClass}
+              />
+            </h1>
+          </div>
+      </div>
+      <Loader type='pacman' />
     </>
   )
 }
